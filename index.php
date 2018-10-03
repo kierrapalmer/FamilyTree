73f12d533fecbd077707aa2cbd76a7ba2d604e69
Kierra Palmer <kierrapalmer@weber.edu>
2018-10-03 16:27:30 -0600

    add spouse

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
-    require_once("db_config.php");
-    $displayRootBtn = true;
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 
+    require_once("db_config.php");
+
+    /*--- User Account ---*/
     session_start();
     if(isset($_SESSION["uId"])) {
         $id = $_SESSION["uId"];
@@ -14,57 +17,88 @@
         die();
     }
 
+    /*--- Select All ---*/
+    $q = $db_connection->prepare("SELECT * FROM tree_node");
+    $q->execute();
+    $r = $q->fetchAll();
+    $count = count($r);
+    $results = $db_connection->query("SELECT * FROM tree_node ORDER BY parentId, _id");
 
-	if(isset($_POST['submitNode'])){
+
+    /*--- Add Node Post Submission ---*/
+    if(isset($_POST['submitNode'])){
         $id = filter_var($_POST['id'], FILTER_SANITIZE_STRING);
         $first = filter_var($_POST['firstname'], FILTER_SANITIZE_STRING);
         $last = filter_var($_POST['lastname'], FILTER_SANITIZE_STRING);
-        $nodeType = filter_var($_POST['nodetype'], FILTER_SANITIZE_STRING);
+        $nodeType = filter_var($_POST['nodeType'], FILTER_SANITIZE_STRING);
         $generation = filter_var($_POST['generation'], FILTER_SANITIZE_STRING);
-        $parentId = filter_var($_POST['parentId'], FILTER_SANITIZE_STRING);
+//        echo $first . " " . $last . " " . $nodeType . " " . $id . " " . $generation;
 
         if($nodeType == "spouse") {
-            $node = new Node($id, $first, $last, $nodeType, $generation, $parentId);
+            $node = new Node($id, $first, $last, $generation, null);
+            $node -> addSpouse($db_connection);
         }
         elseif ($nodeType == "child"){
-            $node = new Node(null, $first, $last, $nodeType, $generation, $id);         //set parentId to incoming id
+            $node = new Node(null, $first, $last, $generation, $id);         //set parentId to incoming id
             $node -> addChild($db_connection);
         }
 
     }
 
+
+    /*--- Node Class ---*/
     class Node {
 	    public $id;
 	    public $first;
 	    public $last;
-	    public $nodeType;
 	    public $generation;
 	    public $parentId;
 
-	    public function __construct($id, $first, $last, $nodeType, $generation, $parentId){
+	    public function __construct($id, $first, $last, $generation, $parentId){
             $this->id = $id;
             $this->first = $first;
 			$this->last = $last;
-			$this->nodeType = $nodeType;
 			$this->generation = $generation;
 			$this->parentId = $parentId;
 		}
 
 		public function addChild($db_connection){
-
-            $query = "INSERT INTO tree_node (_id, firstName, lastName, generation)
-                  VALUES(:_id, :firstName, :lastName, :generation)";
+            $query = "INSERT INTO tree_node (_id, firstName, lastName, generation, parentId)
+                  VALUES(:_id, :firstName, :lastName, :generation, :parentId)";
 
             try{
+                $this->generation++;
                 $result = $db_connection->prepare($query);
                 if($result->execute([
                     '_id'           => null,
                     'firstName'     => $this->first,
                     'lastName'      => $this->last,
                     'generation'    => $this->generation,
-                    'parentId'    => $this->parentId,
+                    'parentId'      => $this->parentId,
                 ])){
-                    $displayRootBtn = false;
+                    header('Location: index.php');
+                    die();
+                }
+                else
+                    echo "There was an error adding the member. Please try again";
+            }
+            catch (Exception $ex){
+                echo "Error adding member." . $ex;
+            }
+        }
+
+        public function addSpouse($db_connection){
+            $query = "UPDATE tree_node 
+                    SET spouse = :spouse
+                    WHERE _id = :_id";
+
+            try{
+                $spouse = $this->first . " " . $this->last;
+                $result = $db_connection->prepare($query);
+                if($result->execute([
+                    '_id'           => $this->id,
+                    'spouse'     => $spouse,
+                ])){
                     header('Location: index.php');
                     die();
                 }
@@ -77,12 +111,7 @@
         }
     }
 
-    $q = $db_connection->prepare("SELECT * FROM tree_node");
-    $q->execute();
-    $r = $q->fetchAll();
-    $count = count($r);
 
-    $results = $db_connection->query("SELECT * FROM tree_node")
 ?>
 
 
@@ -99,7 +128,7 @@
           integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
 	<script>
-		function addNode(id, nodeType) {
+		function addNode(id, gen, nodeType) {
             var f = document.createElement("form");
             f.setAttribute('method',"post");
             f.setAttribute('action',"index.php");
@@ -112,21 +141,20 @@
             last.setAttribute('type',"text");
             last.setAttribute('name',"lastname");
 
-            var id = document.createElement("input");
-            id.setAttribute('type',"hidden");
-            id.setAttribute('name',"id");
+            var i = document.createElement("input");
+            i.setAttribute('type',"hidden");
+            i.setAttribute('name',"id");
+            i.setAttribute('value', id.toString());
 
-            var nodeType = document.createElement("input");
-            nodeType.setAttribute('type',"hidden");
-            nodeType.setAttribute('name',"nodeType");
+            var node = document.createElement("input");
+            node.setAttribute('type',"hidden");
+            node.setAttribute('name',"nodeType");
+            node.setAttribute('value', nodeType.toString());
 
             var generation = document.createElement("input");
             generation.setAttribute('type',"hidden");
             generation.setAttribute('name',"generation");
-
-            var parentId = document.createElement("input");
-            parentId.setAttribute('type',"hidden");
-            parentId.setAttribute('name',"parentId");
+            generation.setAttribute('value', gen.toString());
 
 
             var s = document.createElement("input");
@@ -136,10 +164,9 @@
 
             f.appendChild(first);
             f.appendChild(last);
-            f.appendChild(id);
-            f.appendChild(nodeType);
+            f.appendChild(i);
+            f.appendChild(node);
             f.appendChild(generation);
-            f.appendChild(parentId);
             f.appendChild(s);
             document.getElementById("addNode").appendChild(f);
         }
@@ -165,13 +192,25 @@
     <?php }	?>
 
     <table border="1">
+        <thead>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Spouse</td>
+            <td>Generation</td>
+            <td>Parent</td>
+            <td>Add Spouse</td>
+            <td>Add Child</td>
+        </thead>
         <?php
         foreach($results as $result) {	?>
             <tr>
-                <td><?php echo $result['firstName']?></td>
-                <td><?php echo $result['lastName']?></td>
-                <td><a href="#" onclick="addNode(<?php echo $result['_id']?>, 'spouse')">Add Spouse</a></td>
-                <td><a href="#" onclick="addNode(<?php echo $result['_id']?>, 'child')">Add Child</a></td>
+                <td><?php echo $result['_id']?></td>
+                <td><?php echo $result['firstName'] . " " . $result['lastName']?></td>
+                <td><?php if($result['spouse'] != null){echo $result['spouse'];} else{ echo "-";}?></td>
+                <td><?php echo $result['generation']?></td>
+                <td><?php echo $result['parentId']?></td>
+                <td><a href="#" onclick="addNode(<?php echo $result['_id']?>, <?php echo $result['generation']?>, 'spouse')">Add Spouse</a></td>
+                <td><a href="#" onclick="addNode(<?php echo $result['_id']?>, <?php echo $result['generation']?>, 'child')">Add Child</a></td>
             </tr>
         <?php }	?>
     </table>
